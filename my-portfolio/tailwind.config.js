d28c19165d150ea82b2410b73cb54391874a4112
Quentin <91956889+SadeQL@users.noreply.github.com>
2022-01-14 10:12:04 +0100

    some changes

---
+++ my-portfolio/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/imgs/header-bg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
